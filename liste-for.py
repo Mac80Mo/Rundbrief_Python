50cba50927eb81c743a2295cbe62b310bc0f74d7
Mac80Mo <marcus.moser@mail.de>
2025-02-17 12:20:47 +0100

    Update liste-for.py

--- liste-for.py
+++ liste-for.py
@@ -4,7 +4,13 @@ names = [
     "Tobias M."
 ]
 
-text = "Ich bin ein Text"
+#text = "Ich bin ein Text"
 
 for name in names:
-    print("Einladung an: " + name)
+    print("Einladung an: " + name + "\n\n")
+    text = f"Hallo {name}, \n\n" + \
+        "Ich möchte Dich gerne zu meinem Geburtstag einladen.\n\n" + \
+        "Viele Grüße\n\n" + \
+        "Marcus"
+    print(text)
+    print("----------------")
