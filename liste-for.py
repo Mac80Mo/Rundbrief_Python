47609dfcc44ba2bc6fcaa5f578834bf13bc0e1f6
Mac80Mo <marcus.moser@mail.de>
2025-02-18 19:51:11 +0100

    rundbrief fertig

--- liste-for.py
+++ liste-for.py
@@ -1,12 +1,21 @@
+import os
+
 with open("gaeste.txt", "r", encoding="utf-8") as file:
     for name in file:
         name = name.strip()
         print("Einladung an: " + name + "\n\n")
+        
         text = f"Hallo {name}, \n\n" + \
             "Ich möchte Dich gerne zu meinem Geburtstag einladen.\n\n" + \
             "Viele Grüße\n\n" + \
             "Marcus"
+        
+        pathToNewFile = os.path.join("einladungen", f"{name}.txt")
+        with open(pathToNewFile, "w", encoding="utf-8") as outFile:
+            outFile.write(text)
+        
+        
         print(text)
-        print("----------------")
+
 
 
