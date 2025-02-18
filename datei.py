5fe764d35ecd06625fc73a3dc32ae612e3ba9e53
Mac80Mo <marcus.moser@mail.de>
2025-02-18 19:12:09 +0100

    weitergearbeitet

---
+++ datei.py
@@ -0,0 +1,5 @@
+
+with open('gaeste.txt', 'r', encoding='utf-8') as file:
+    #print(file.read())
+    for line in file:
+        print(line)
--- liste-for.py
+++ liste-for.py
@@ -1,16 +1,12 @@
-names = [
-    "Moritz Mueller",
-    "Erika Mustermann",
-    "Tobias M."
-]
+with open("gaeste.txt", "r", encoding="utf-8") as file:
+    for name in file:
+        name = name.strip()
+        print("Einladung an: " + name + "\n\n")
+        text = f"Hallo {name}, \n\n" + \
+            "Ich möchte Dich gerne zu meinem Geburtstag einladen.\n\n" + \
+            "Viele Grüße\n\n" + \
+            "Marcus"
+        print(text)
+        print("----------------")
 
-#text = "Ich bin ein Text"
 
-for name in names:
-    print("Einladung an: " + name + "\n\n")
-    text = f"Hallo {name}, \n\n" + \
-        "Ich möchte Dich gerne zu meinem Geburtstag einladen.\n\n" + \
-        "Viele Grüße\n\n" + \
-        "Marcus"
-    print(text)
-    print("----------------")
